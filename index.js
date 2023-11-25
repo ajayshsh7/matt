b9aba09c682ac47c057052a10ba1cf3bfe319c21
ajayshsh7 <ajayshsh7@gmail.com>
2023-11-25 11:39:39 +0530

    index.js file modified

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
